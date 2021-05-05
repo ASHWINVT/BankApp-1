651eb88edeb2cf13048c7f886c48026f91ea2ee6
ASHWINVT <ashwinvallilthodi02@gmail.com>
2021-05-05 22:59:17 +0530

    login added and binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,10 +9,45 @@ export class LoginComponent implements OnInit {
 
   aim ="YOUR PERFECT BANKING PARTNER"
   account = "account number"
+  accno = " "
+  pswd = " "
+  accountDetails:any = {
+    1000: { acno: 1000, pswd: "user1", balance: 5000,uname:"user1"  },
+    1001: { acno: 1001, pswd: "user2", balance: 3000 ,uname:"user2"},
+    1002: { acno: 1002, pswd: "user3", balance: 10000 ,uname:"user3"},
+    1003: { acno: 1003, pswd: "user4", balance: 1000 ,uname:"user4"},
+}
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  changeAcc(event:any){
+    this.accno = event.target.value;
+    console.log(this.accno)
+  }
+  changePswd(event:any){
+    this.pswd = event.target.value;
+    console.log(this.pswd)
+  }
+  login(){
+    var acno = this.accno;
+    var pswd = this.pswd;
+    let dataset = this.accountDetails;
+    if (acno in dataset) {
+        if (pswd == dataset[acno]["pswd"]) 
+        {
+          alert("Login Successfull");
+        }
+        else 
+        {
+          alert("Invalid Password");
+        }
 
+    }
+    else
+    {
+      alert("Invalid Account Number Please Register");
+    }
+  }
 }
